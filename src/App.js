36584ffa8fdae3ddaabe5d5a384b8f60b3ee896a
wizgurl101 <nb.phan@hotmail.com>
2021-05-05 14:24:05 -0600

    add ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
+/**
+ * Expense Tracker Application
+ */
 function App() {
+  // test data
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 299.99,
+      date: new Date(),
+    },
+    {
+      id: "e2",
+      title: "Life Insurance",
+      amount: 9.99,
+      date: new Date(),
+    },
+    {
+      id: "e3",
+      title: "Netflix",
+      amount: 9.99,
+      date: new Date(),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+    </>
   );
 }
 
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+
+/**
+ * Expense Item component
+ */
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <div>{props.date.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">${props.amount}</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
